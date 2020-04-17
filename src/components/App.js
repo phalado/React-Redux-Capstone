5a0bbdb8b876a8405b295e017100de792ccd4e40
phalado <phalado@gmail.com>
2020-04-17 09:41:09 -0300

    Added hooks to the project, but I'm receiveing an error in useEffects

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import HeroesList from '../containers/HeroesList';
 import HeroFile from './HeroFile';
@@ -12,73 +13,76 @@ import Home from './Home';
 import About from './About';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddHero = this.handleAddHero.bind(this);
-    this.clickHandler = this.clickHandler.bind(this);
-    this.state = {
-      heroes: [],
-    };
-  }
+const App = props => {
+  const [heros, setHeros] = useState();
+  const {
+    chngFilter, heroes, filter, addNewHero,
+  } = props;
 
-  componentDidMount() {
-    fetch('https://superheroapi.com/api.php/2602016920125978/search/_')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          heroes: json.results.filter(heroes => heroes.biography.publisher === 'DC Comics'
+  const handleAddHero = () => {
+    if (heros) {
+      heros.heroes.forEach(value => {
+        addNewHero(value);
+      });
+    }
+  };
+
+  useEffect(() => {
+    let mounted = true;
+
+    const loadHeroes = async () => {
+      const response = await axios.get('https://superheroapi.com/api.php/2602016920125978/search/_');
+
+      if (mounted) {
+        setHeros({
+          heroes: response.data.results.filter(heroes => heroes.biography.publisher === 'DC Comics'
             || heroes.biography.publisher === 'Superman Prime One-Million'
             || heroes.biography.publisher === 'Black Racer'),
         });
-      })
-      .then(this.handleAddHero);
-  }
 
-  handleAddHero() {
-    const { addNewHero } = this.props;
-    const { heroes } = this.state;
-    heroes.forEach(hero => {
-      addNewHero(hero);
-    });
-  }
+        handleAddHero();
+      }
+    };
 
-  clickHandler(value) {
-    const { chngFilter } = this.props;
+    loadHeroes();
+
+    return () => {
+      mounted = false;
+    };
+  }, heros);
+
+  const clickHandler = value => {
     chngFilter(value);
-  }
+  };
 
-  render() {
-    const { heroes, filter } = this.props;
-    return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/heroesList">
-              <HeroesList />
-            </Route>
-            <Route path="/teamsList">
-              <TeamsList />
-            </Route>
-            <Route path="/heroFile/:id">
-              <HeroFile
-                hero={heroes.filter(hero => hero.id === filter.value[1])[0]}
-                filter={filter}
-                handleClick={this.clickHandler}
-              />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/heroesList">
+            <HeroesList />
+          </Route>
+          <Route path="/teamsList">
+            <TeamsList />
+          </Route>
+          <Route path="/heroFile/:id">
+            <HeroFile
+              hero={heroes.filter(hero => hero.id === filter.value[1])[0]}
+              filter={filter}
+              handleClick={clickHandler}
+            />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 App.propTypes = {
   addNewHero: PropTypes.func.isRequired,
